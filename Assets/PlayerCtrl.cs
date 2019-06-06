9170d8aa2d0ffae2ca01177125c5d79f00cd24c8
SuperNach0 <erwan@tertrais.xyz>
2019-06-06 12:36:42 +0200

    double jump done

--- Assets/PlayerCtrl.cs
+++ Assets/PlayerCtrl.cs
@@ -6,23 +6,41 @@ public class PlayerCtrl : MonoBehaviour {
 
 	public float speed;
 	public float jumpVelocity;
+	public float nb_jumps_max;
+	float nb_jumps = 0;
+	bool can_jump = true;
+	bool grounded = true;
+
 
 	Rigidbody2D rb;
 
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody2D> ();
-
 	}
 
 	// Update is called once per frame
 	void Update () {
+
 		float move = Input.GetAxis ("Horizontal");
 		rb.velocity = new Vector2 (speed*move,rb.velocity.y);
-		if (Input.GetButtonDown ("Jump")){
-			rb.velocity = Vector2.up*jumpVelocity;
-		}
 
+		// let's make the jumps :)
+		can_jump = grounded || nb_jumps < nb_jumps_max;
+
+
+		if (Input.GetButtonDown ("Jump") && can_jump){
+			rb.velocity = Vector2.up*jumpVelocity;
+			nb_jumps ++;
+			grounded = false;
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D coll){
+	        if(coll.gameObject.tag == "Ground"){
+						grounded = true;
+						nb_jumps=0;
+					}
 	}
 
 	void FixedUpdate(){
